2ee0e3412fc7fa41f0e3fde172b68b53b14491aa
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-11-09 12:22:56 -0700

    add magnetics modeling options

---
+++ magnetics/mag_prism.py
@@ -0,0 +1,206 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import HTML, VBox, Label, Widget, FloatSlider, HBox, IntSlider, Layout, ToggleButtons, Output, Button
+
+from geoana.em.static import MagneticPrism
+from scipy.constants import mu_0
+from SimPEG.potential_fields.magnetics.analytics import IDTtoxyz
+
+class MagneticPrismInteract():
+
+    def __init__(self, grid_east=None, grid_north=None, grid_height=None, prism=None):
+
+        if grid_east is None:
+            grid_east = np.linspace(-1000,1000,101)
+        if grid_north is None:
+            grid_north = np.linspace(-1000,1000,101)
+        if grid_height is None:
+            grid_height = 0
+        if prism is None:
+            prism = [[-100, -100, -300],[100, 100, -100]]
+        self._grid_x = np.asarray(grid_north)
+        self._grid_y = np.asarray(grid_east)
+        self._grid_z = -grid_height
+        x, y, z = np.meshgrid(self._grid_x, self._grid_y, [self._grid_z])
+        self._obs = np.stack([x[..., 0], y[..., 0], z[..., 0]], axis=-1)
+
+        self.prism = MagneticPrism(*prism)
+        
+
+        # toggle hemisphere
+        self._hemi_toggle = ToggleButtons(
+            options=['North', 'South'],
+            description='Hemisphere',
+            disabled=False,
+            button_style='',
+            tooltips=['Which hemisphere is your prism in?',],
+            layout=Layout(width='200px')
+        )
+
+        # toggle component
+        self._comp_toggle = ToggleButtons(
+            options=['TFA', 'East', 'North', 'Down'],
+            description='Component',
+            disabled=False,
+            button_style='',
+            tooltips=['Which component would you like to plot?',],
+            layout=Layout(width='200px')
+        )
+
+        with plt.ioff():
+            #fig, ax_polar = plt.subplots(subplot_kw={'projection': 'polar'})
+            #fig, [ax_polar, ax_image] = plt.subplots(1, 2, subplot_kw={'projection': 'polar'})
+            fig = plt.figure(figsize=(5,2))
+            ax_polar = plt.subplot(121, projection='polar')
+            ax_image = plt.subplot(122)
+
+                
+        toolbar = plt.get_current_fig_manager().toolbar
+        scatter = ax_polar.scatter([0],[90])
+        ax_polar.set_rlim(bottom=90, top=0)
+        ax_polar.grid(True)
+        ax_polar.set_theta_zero_location("N")
+        ax_polar.set_theta_direction(-1)
+        ax_polar.set_xlabel('Declination')
+
+        self._scatter = scatter
+
+        self._magnitude_slider = FloatSlider(
+            value=50000,
+            min=25000,
+            max=65000,
+            step=1,
+            description='|B| (nT)',
+            disabled=False,
+            continuous_update=True,
+            orientation='vertical',
+            readout=True,
+            readout_format='.1f',
+        )
+
+        self._sus_slider = FloatSlider(
+            value=0.1,
+            min=0,
+            max=1,
+            step=0.01,
+            description='Susceptibility (SI)',
+            disabled=False,
+            continuous_update=True,
+            orientation='vertical',
+            readout=True,
+            readout_format='.2f',
+        )
+
+        B, ba = self._do_forward()
+        # setup data plot
+        # with plt.ioff():
+        #     fig2, ax_image = plt.subplots()
+        im_ba = ax_image.pcolormesh(grid_east, grid_north, ba)
+        ax_image.set_aspect('equal', 'box')
+        ax_image.set_xlabel('Easting (m)')
+        ax_image.set_ylabel('Northing (m)')
+        cb = plt.colorbar(im_ba)
+        cb.set_label('Total Field Anomaly (nT)')
+        self._im_ba = im_ba
+        self._ax_image = ax_image
+        self._cb_image = cb
+
+        def toggle_hemi(event):
+            if self._hemi_toggle.value == 'North':
+                data = scatter.get_offsets()
+                data[:, 1] = np.abs(data[:, 1])
+                ax_polar.set_rlim(bottom=90, top=0)
+                scatter.set_offsets(data)
+            else:
+                data = scatter.get_offsets()
+                data[:, 1] = -np.abs(data[:, 1])
+                ax_polar.set_rlim(bottom=-90, top=0)
+                scatter.set_offsets(data)
+            self._update_image()
+
+        def slider_update(event):
+            self._update_image()
+                
+        self._hemi_toggle.on_trait_change(toggle_hemi)
+        self._magnitude_slider.on_trait_change(slider_update)
+        self._sus_slider.on_trait_change(slider_update)
+        self._comp_toggle.on_trait_change(slider_update)
+        
+        self.__dragging = False
+        # # setup function for clicking a line:
+        # # Define a function for handling button press events
+        def on_press(event):
+            if event.inaxes == ax_polar and toolbar.mode != 'pan/zoom':
+                contains, attrd = scatter.contains(event)
+                if contains:
+                    self.__dragging = True
+        
+        # Define a function for handling mouse motion events
+        def on_motion(event):
+            if self.__dragging and event.inaxes == ax_polar and toolbar.mode != 'pan/zoom':
+                x, y = event.xdata, event.ydata
+                if self._hemi_toggle.value == 'North': 
+                    y = 180 + y
+                scatter.set_offsets([x, y])
+                self._update_image()
+                # update data plot
+        
+        # Define a function for handling button release events
+        def on_release(event):
+            self.__dragging = False
+        
+        # # Connect the event handlers
+        fig.canvas.mpl_connect('button_press_event', on_press)
+        fig.canvas.mpl_connect('motion_notify_event', on_motion)
+        fig.canvas.mpl_connect('button_release_event', on_release)
+        slider_box = HBox([self._magnitude_slider, self._sus_slider])
+
+        vbox = VBox([self._hemi_toggle, slider_box])
+        box2 = HBox([vbox, self._comp_toggle])
+
+        box = HBox([box2, fig.canvas])
+
+        #big_box = VBox([box, fig2.canvas])
+
+        self._box = box
+
+    def _do_forward(self):
+        
+        data = self._scatter.get_offsets()
+        dec = data[0,0] * 180/np.pi
+        inc = data[0,1]
+
+        strength = self._magnitude_slider.value
+        sus = self._sus_slider.value
+        b0 = IDTtoxyz(inc, dec, strength)
+        #b0 = np.r_[b0[1], b0[0], b0[2]]
+        M = b0 / mu_0 * sus
+        
+        self.prism.magnetization = M
+        mag_vec = self.prism.magnetic_flux_density(self._obs)
+        ba = mag_vec.dot(b0) / strength
+        return mag_vec, ba
+
+    def _update_image(self):
+        B, ba = self._do_forward()
+        if self._comp_toggle.value == 'East':
+            b = -B[..., 0]
+            label = r"$B_{east}$ (nT)"
+        elif self._comp_toggle.value == 'North':
+            b = -B[..., 1]
+            label = r"$B_{north}$ (nT)"
+        elif self._comp_toggle.value == 'Down':
+            b = B[..., 2]
+            label = r"$B_{down}$ (nT)"
+        else:
+            b = ba
+            label = r"Total Field Anomaly (nT)"
+            
+        self._im_ba.update({
+            'array': b.ravel(),
+            'clim':[b.min(), b.max()],
+        })
+        self._cb_image.set_label(label)
+
+    def display(self):
+        return self._box
---
+++ magnetics/mpoly.py
@@ -0,0 +1,296 @@
+# Compute the gravity response of a polygon
+# Originally version of gpoly adapted from Blakely (1995) by Yaoguo Li for GPGN-303 Gravity and Magnetics
+# Expanded for multiple geologic sources and tunnel detection by Richard Krahenbuhl
+# for GPGN-303 (Gravity, Magnetics, Electrical) GPGN-304 (Gravity & Magnetics) & GPGN-314 (Applied Geophysics)
+# Updated in Python by Andy McAliley for GPGN-304 (Gravity & Magnetics)
+# Continued updates by Gavin Wilson during GPGN-314 (Applied Geophysics)
+# Added docstring by Joe Capriotti
+# fix for true limits (no need to adjust small numbers) by Joe Capriotti
+# vectorized call over observations by Joe Capriotti
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import patches
+from scipy.constants import mu_0
+
+def mpoly(obs, nodes, magnetization, proj_dir=None):
+    """Calculate the magnetic flux density of an infinite 2D polygonal prism.
+
+    This function computes the magnetic flux density at observation points
+    due to a polygonal body defined by a set of nodes using the method
+    described in Blakely, 1996. It optionally will also project the anomaly
+    along a specific direction.
+
+    Parameters
+    ----------
+    obs : (n_loc, 2) numpy.ndarray
+        Observation locations in meters (x, z) (z positive down).
+    nodes : (n_nodes, 2) numpy.ndarray
+        Polygon node locations in meters (x, z), must be listed in clockwise order.
+    magnetization : (2, ) numpy.ndarray
+        Magnetization of the the prism (nA/m).
+    proj_dir : (2, ) numpy.ndarray, optional
+        Direction to project the anomaly onto.
+
+    Returns
+    -------
+    B : (n_loc, 3) numpy.ndarray,
+        The magnetic flux density, and total field anomaly in units of nT
+        (ordered as [Bx, Bz, TFA]).
+
+    Notes:
+    ------
+    The function uses the formula described in Blakely, 1996, to compute the magnetic flux density
+    attraction.
+
+    References:
+    -----------
+    1. Blakely, R. J. (1996). Potential theory in gravity and magnetic applications.
+       Cambridge University Press.
+
+    Example:
+    --------
+    >>> obs = np.array([[1.0, 2.0], [3.0, 4.0]])
+    >>> nodes = np.array([[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]])
+    >>> density = 2.5  # grams per cubic centimeter (g/cm³)
+    >>> grav = gpoly(obs, nodes, density)
+    """
+    #Blakely, 1996
+    gamma = 6.672E-03 # mGal
+    obs = np.asarray(obs)
+    nodes = np.asarray(nodes)
+    
+    # append a value to obs to do a cursory test for CW vs CCW
+    # basically add a point outside and at a lower z-value than the body.
+    # This point is above the center of the object.
+    
+    obs = np.r_[obs, [[np.median(nodes[:, 0]), np.min(nodes[:, 1]) - 100]]]
+    
+    numobs = len(obs)
+    numnodes = len(nodes)
+    b = np.zeros((numobs, 2))
+    
+    for i1 in range(numnodes):
+        i2 = (i1 + 1) % numnodes
+
+        gzx_gzz = fz_segment_dxz(obs, nodes[i1], nodes[i2])
+        gxx_gxz = -fz_segment_dxz(obs[:, ::-1], nodes[i1][::-1], nodes[i2][::-1])[:, ::-1]
+
+        bx = gxx_gxz.dot(magnetization)
+        bz = gzx_gzz.dot(magnetization)
+        b += np.c_[bx, bz]
+
+    if magnetization[1] != 0:
+        # the field above the polygon should have the same direction z component.
+        if np.sign(b[-1, 1]) != np.sign(magnetization[1]):
+            b *= -1
+    else:
+        # the x component should reverse outside the body directly above it.
+        if -np.sign(b[-1, 0]) != np.sign(magnetization[0]):
+            b *= -1
+    b = b[:-1]
+    b *= mu_0 / (4 * np.pi)
+    if proj_dir is None:
+        proj_dir = magnetization
+    proj_dir = proj_dir / np.linalg.norm(proj_dir)
+    return np.c_[b, b.dot(proj_dir)]
+        
+
+def fz_segment_dxz(obs, node_a, node_b):
+    dr1 = node_a - obs
+    dr2 = node_b - obs
+    segment = node_a - node_b
+    a = dr1[:, 1]
+    b = dr1[:, 0]
+    c = dr2[:, 1]
+    d = dr2[:, 0]
+
+    dtheta = _dtheta(a, b, c, d)
+
+    flat_line = segment[1] == 0
+    if flat_line:
+        g_dtheta  = dr1[:, 1]
+        g_dx1 = np.zeros_like(dtheta)
+        g_dz1 = dtheta
+        g_a = np.zeros_like(a)
+        g_b = np.zeros_like(b)
+        g_c = np.zeros_like(c)
+        g_d = np.zeros_like(d)
+    else:
+        r1sq = a * a + b * b
+        r2sq = c * c + d * d
+
+        off_nodes = (r1sq != 0) & (r2sq != 0)
+        r1sq = r1sq[off_nodes]
+        r2sq = r2sq[off_nodes]
+        
+        alpha = segment[0]/segment[1]
+        beta = dr1[:, 0] - alpha * dr1[:, 1]
+        
+        term1 = np.zeros_like(beta)
+        term1[off_nodes] = 0.5 * np.log(r2sq / r1sq)
+        term2 = alpha * dtheta
+        factor = beta / (1 + alpha ** 2)
+
+        factor[~off_nodes] = 0.0
+
+        #contribution = factor * (term1 - term2)
+        g_factor = (term1 - term2)
+        g_term1 = factor
+        g_term2 = -factor
+        #factor = beta / (1 + alpha ** 2)
+        g_beta = 1/(1 + alpha ** 2) * g_factor
+        #term2 = alpha * dtheta
+        g_dtheta = alpha * g_term2
+        #term1[off_nodes] = 0.5 * np.log(r2sq / r1sq)
+        g_r2sq = np.zeros_like(beta)
+        g_r1sq = np.zeros_like(beta)
+
+        g_r2sq[off_nodes] = 1 / (2 * r2sq) * g_term1[off_nodes]
+        g_r1sq[off_nodes] = -1 / (2 * r1sq) * g_term1[off_nodes]
+        #beta = dr1[:, 0] - alpha * dr1[:, 1]
+        g_dx1 = g_beta
+        g_dz1 = - alpha * g_beta
+        # r1sq = a * a + b * b
+        g_a = 2 * a * g_r1sq
+        g_b = 2 * b * g_r1sq
+        # r2sq = c * c + d * d
+        g_c = 2 * c * g_r2sq
+        g_d = 2 * d * g_r2sq
+
+    #dtheta = _dtheta(a, b, c, d)
+    inds = b != 0
+    g_a[inds] += _dtheta_da(a[inds], b[inds], c[inds], d[inds]) * g_dtheta[inds]
+    inds = a != 0
+    g_b[inds] += _dtheta_db(a[inds], b[inds], c[inds], d[inds]) * g_dtheta[inds]
+    inds = d != 0
+    g_c[inds] += _dtheta_dc(a[inds], b[inds], c[inds], d[inds]) * g_dtheta[inds]
+    inds = c != 0
+    g_d[inds] += _dtheta_dd(a[inds], b[inds], c[inds], d[inds]) * g_dtheta[inds]
+
+    g_dx2 = g_d
+    g_dz2 = g_c
+    g_dx1 += g_b
+    g_dz1 += g_a
+
+    g_obs_x = - (g_dx2 + g_dx1)
+    g_obs_z = - (g_dz2 + g_dz1)
+
+    return np.c_[g_obs_x, g_obs_z]
+    
+
+def _dtheta(a, b, c, d):
+    return np.arctan2(b * c - a * d, b * d + a * c)
+
+def _dtheta_da(a, b, c, d):
+    return -b/(a * a + b * b)
+
+def _dtheta_db(a, b, c, d):
+    return a/(a * a + b * b)
+
+def _dtheta_dc(a, b, c, d):
+    return d/(c * c + d * d)
+
+def _dtheta_dd(a, b, c, d):
+    return -c/(c * c + d * d)
+
+def plot_model(data, obs_locs, *polygons, show_locations=True, loc_size=0.2):
+    """
+    Plot geophysical data and geological models with dikes.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of geophysical data values.
+    obs_locs : array_like
+        Array of observation locations.
+    *polygons : variable number of arrays
+        Arrays representing the dikes as polygons.
+    show_locations : bool
+        Whether to show the observation locations on the model plot
+    loc_size : float
+        Size of the observation points (if show_locations is True).
+
+    Returns
+    -------
+    ax_data, ax_model : matplotlib.axes.Axes
+        axes for the data and model plots.
+
+    Notes
+    -----
+    This function creates a two-panel plot with the top panel showing
+    geophysical data and the bottom panel displaying geological models
+    with dikes represented as polygons.
+
+    The top panel includes the following elements:
+    - x-axis labeled as 'Position (m)'
+    - y-axis labeled as 'g_D (mGal)'
+    - Plot of geophysical data at observation locations.
+
+    The bottom panel includes the following elements:
+    - x-axis labeled as 'Position (m)'
+    - y-axis labeled as 'Depth (m)'
+    - Polygons represented as patches.
+    - Y-axis inverted to display depth increasing downwards.
+
+    Examples
+    --------
+    >>> data = [10, 20, 30, 40, 50]
+    >>> obs_locs = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
+    >>> dike1 = np.array([[1, 2], [2, 3], [3, 4]])
+    >>> dike2 = np.array([[4, 5], [5, 6], [6, 7]])
+    >>> plot_model(data, obs_locs, dike1, dike2)
+    """
+
+    fig = plt.figure()
+    ax_data = fig.add_subplot(2,1,1)
+    plt.xlabel('Position (m)')
+    ax_data.set_ylabel(r'$\vec{B}$ (nT)')
+
+    ax_model = fig.add_subplot(2,1,2,sharex=ax_data)
+    ax_model.invert_yaxis()
+    ax_model.set_ylabel('Depth (m)')
+    ax_model.set_xlabel('Position (m)')
+    
+    if show_locations:
+        ax_model.scatter(obs_locs[:, 0], obs_locs[:, 1], s=loc_size)
+
+    ndim = data.ndim
+    if ndim > 1:
+        labels = ['Bx', 'Bz', 'TFA']
+        for i in range(data.shape[1]):
+            ax_data.plot(obs_locs[:, 0], data[:, i], label=labels[i])
+        ax_data.legend()
+    else:
+        ax_data.plot(obs_locs[:, 0], data)
+
+    for poly in polygons:
+        polygon = patches.Polygon(poly)
+        ax_model.add_patch(polygon)
+    ax_model.use_sticky_edges = False
+    ax_model.set_ymargin(0.1)
+    ax_model.autoscale()
+    return ax_data, ax_model
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    xmin = 0.
+    xmax = 10.
+    nx = 201
+
+    obs = np.zeros((nx,2))
+    obs[:,0] = np.linspace(xmin,xmax,nx)
+
+    nodes = np.zeros((4,2))
+    nodes[0] = [2.,1.5]
+    nodes[1] = [3.,1.5]
+    nodes[2] = [3.,2.5]
+    nodes[3] = [2.,2.5]
+
+    density = 1
+
+    grav = gpoly(obs,nodes,density)
+
+    plt.plot(obs[:,0],grav)
+    plt.show()
